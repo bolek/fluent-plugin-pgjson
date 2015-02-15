92e61731a32b464015b7aa6f051a62f54567d59b
Bolek <bolek@meyouhealth.com>
2015-02-15 13:13:17 -0500

    Refactor and reformat plugin

--- lib/fluent/plugin/out_pgjson.rb
+++ lib/fluent/plugin/out_pgjson.rb
@@ -1,23 +1,23 @@
 module Fluent
 
 class PgJsonOutput < Fluent::BufferedOutput
-  Fluent::Plugin.register_output('pgjson', self)
+  Fluent::Plugin.register_output("pgjson", self)
 
-  config_param :host       , :string  , :default => 'localhost'
-  config_param :port       , :integer , :default => 5432
-  config_param :sslmode    , :string  , :default => 'prefer'
+  config_param :host       , :string  , default: "localhost"
+  config_param :port       , :integer , default: 5432
+  config_param :sslmode    , :string  , default: "prefer"
   config_param :database   , :string
   config_param :table      , :string
-  config_param :user       , :string  , :default => nil
-  config_param :password   , :string  , :default => nil
-  config_param :time_col   , :string  , :default => 'time'
-  config_param :tag_col    , :string  , :default => 'tag'
-  config_param :record_col , :string  , :default => 'record'
-  config_param :msgpack    , :bool    , :default => false
+  config_param :user       , :string  , default: nil
+  config_param :password   , :string  , default: nil
+  config_param :time_col   , :string  , default: "time"
+  config_param :tag_col    , :string  , default: "tag"
+  config_param :record_col , :string  , default: "record"
+  config_param :msgpack    , :bool    , default: false
 
   def initialize
     super
-    require 'pg'
+    require "pg"
     @conn = nil
   end
 
@@ -28,9 +28,8 @@ class PgJsonOutput < Fluent::BufferedOutput
   def shutdown
     super
 
-    if ! @conn.nil? and ! @conn.finished?
-      @conn.close()
-    end
+    return if !conn.nil? && !conn.finished?
+    conn.close
   end
 
   def format(tag, time, record)
@@ -38,47 +37,64 @@ class PgJsonOutput < Fluent::BufferedOutput
   end
 
   def write(chunk)
-    init_connection
-    @conn.exec("COPY #{@table} (#{@tag_col}, #{@time_col}, #{@record_col}) FROM STDIN WITH DELIMITER E'\\x01'")
+    conn.exec(copy_cmd)
     begin
-      chunk.msgpack_each do |tag, time, record|
-        @conn.put_copy_data "#{tag}\x01#{Time.at(time).to_s}\x01#{record_value(record)}\n"
-      end
+      chunk.msgpack_each { |tag, time, record| put_copy_data(tag, time, record) }
     rescue => err
-      errmsg = "%s while copy data: %s" % [ err.class.name, err.message ]
-      @conn.put_copy_end( errmsg )
-      @conn.get_result
-      raise
+      handle_error(err)
     else
-      @conn.put_copy_end
-      res = @conn.get_result
-      raise res.result_error_message if res.result_status!=PG::PGRES_COMMAND_OK
+      finish_write
     end
   end
 
   private
-  def init_connection
-    if @conn.nil?
-      $log.debug "connecting to PostgreSQL server #{@host}:#{@port}, database #{@database}..."
 
-      begin
-        @conn = PGconn.new(:dbname => @database, :host => @host, :port => @port, :sslmode => @sslmode, :user => @user, :password => @password)
-      rescue
-        if ! @conn.nil?
-          @conn.close()
-          @conn = nil
-        end
-        raise "failed to initialize connection: #$!"
+  def finish_write
+    conn.put_copy_end
+    res = conn.get_result
+    raise res.result_error_message if res.result_status != PG::PGRES_COMMAND_OK
+  end
+
+  def handle_error(err)
+    errmsg = sprintf("%s while copy data: %s", err.class.name, err.message)
+    conn.put_copy_end(errmsg)
+    conn.get_result
+    raise
+  end
+
+  def copy_cmd
+    "COPY #{@table} (#{@tag_col}, #{@time_col}, #{@record_col}) FROM STDIN WITH DELIMITER E'\\x01'"
+  end
+
+  def copy_data(tag, time, record)
+    "#{tag}\x01#{Time.at(time)}\x01#{record_value(record)}\n"
+  end
+
+  def put_copy_data(tag, time, record)
+    conn.put_copy_data copy_data(tag, time, record)
+  end
+
+  def conn
+    return @conn unless @conn.nil?
+
+    log.debug "connecting to PostgreSQL server #{@host}:#{@port}, database #{@database}..."
+    begin
+      @conn = PGconn.new(dbname: @database, host: @host, port: @port, sslmode: @sslmode, user: @user, password: @password)
+    rescue
+      unless @conn.nil?
+        @conn.close
+        @conn = nil
       end
+      raise "failed to initialize connection: #{$ERROR_INFO}"
     end
   end
 
   def record_value(record)
     if @msgpack
-      "\\#{@conn.escape_bytea(record.to_msgpack)}"
+      "\\#{conn.escape_bytea(record.to_msgpack)}"
     else
       json = record.to_json
-      json.gsub!(/\\/){ '\\\\' }
+      json.gsub!(/\\/) { '\\\\' }
       json
     end
   end
